440d221c27b3700bd6828e8038c2fa2db7dfb8e5
HybridDog <adresse_test@t-online.de>
2014-10-19 10:56:13 +0200

    ...update

--- init.lua
+++ init.lua
@@ -1,40 +1,50 @@
---V1
+--V2
 local function get_random(a, b, seed)
 	return PseudoRandom(math.abs(a+b*5)+seed)
 end
 
 local r_chs = {}
 
-local function hnoise_single(minp, s, seed, range, scale)
+local function hnoise_single(t)
+	local s = t.scale
 	if not r_chs[s] then
 		r_chs[s] = math.floor(s/3+0.5)
 	end
-	scale = scale or 15
+	local minp = t.minp
+	local seed = t.seed
+	local size = t.size
+	--local height = t.height or 10
+
+	size = size or 15
 	local r_ch = r_chs[s]
-	local maxp = vector.add(minp, scale)
+	local maxp = vector.add(minp, size)
 
 	local tab = {}
-	local sm = range or (s+r_ch)*2
-	for z = -sm, scale+sm do
+	local sm = t.range or (s+r_ch)*2
+	for z = -sm, size+sm do
 		local pz = z+minp.z
 		if pz%s == 0 then
-			for x = -sm, scale+sm do
+			for x = -sm, size+sm do
 				local px = x+minp.x
 				if px%s == 0 then
 					local pr = get_random(px, pz, seed)
 					local pstring = px+pr:next(-r_ch, r_ch).." "..pz+pr:next(-r_ch, r_ch)
-					tab[pstring] = pr:next(0, 10)
+					tab[pstring] = pr:next(0, s)
 				end
 			end
 		end
 	end
 
+	s = s+r_ch
+
+	local s_max = math.sqrt(2)*s
+
 	local tab2,n = {},1
 	for z = minp.z, maxp.z do
 		for x = minp.x, maxp.x do
 			local h = 0
-			for z2 = -10, 10 do
-				for x2 = -10, 10 do
+			for z2 = -s, s do
+				for x2 = -s, s do
 					local h2 = tab[x+x2.." "..z+z2]
 					if h2 then
 						--local dist = math.max(math.abs(z2), math.abs(x2))
@@ -45,6 +55,7 @@ local function hnoise_single(minp, s, seed, range, scale)
 					end
 				end
 			end
+			h = h/s_max-0.5
 			--tab2[n] = {x=x, y=maxp.y-h, z=z}
 			tab2[n] = {x=x, y=h, z=z}
 			n = n+1
@@ -53,11 +64,13 @@ local function hnoise_single(minp, s, seed, range, scale)
 	return tab2
 end
 
-local function hnoise(minp, s, seed, range, scale)
-	local n1 = hnoise_single(minp, s, seed, range, scale)
-	local n2 = hnoise_single(minp, s, seed+1, range, scale)
+local function hnoise(t)
+	local n1 = hnoise_single(t)
+	t.seed = t.seed+1
+	local n2 = hnoise_single(t)
+	t.seed = t.seed-1
 	for i = 1,#n1 do
-		n1[i].y = (n1[i].y+n2[i].y)/2
+		n1[i].y = n1[i].y+n2[i].y
 	end
 	return n1
 end
@@ -86,8 +99,8 @@ minetest.register_node(":ac:hmg", {
 	sounds = default_stone_sounds,
 	on_construct = function(pos)
 		local minp = vector.chunkcorner(pos)
-		for _,p in pairs(hnoise(minp, 10, 8)) do
-			p.y = minp.y+p.y
+		for _,p in pairs(hnoise({minp=minp, scale=10, seed=8})) do
+			p.y = minp.y+p.y*10
 			for y = p.y, p.y+3 do
 				p.y = y -- might not work with luajit
 				minetest.set_node(p, {name="default:desert_stone"})
